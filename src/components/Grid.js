78278393007f4b9254c3c7272d1e1df7cd63bf01
Quang Nguyen The <quangnguyenthe@Quangs-MacBook-Air.local>
2022-12-12 11:15:10 +0700

    added live letter rendering

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,7 +5,10 @@ export default function Grid({ currentGuess, guesses, turn }) {
   return (
     <div>
       {guesses.map((guess, i) => {
-        return <Row key={i} guess={guess} />;
+        if(turn === i) { //only pass the currentGuess in on 1 row - the row that is being selected on that turn. else, 1 letter typed will be shown in 6 rows
+            return <Row key={i} currentGuess={currentGuess} />; //render current guesses live
+        }
+        return <Row key={i} guess={guess} />; //render past guesses
       })}
     </div>
   );
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-export default function Row({ guess }) {
-  if (guess) { //if guess has a value then render this
+export default function Row({ guess, currentGuess }) {
+  if (guess) {//render past guess after enter
+    //if guess has a value then render this
     return (
       <div className="row past">
         {guess.map((letter, i) => (
@@ -13,7 +14,25 @@ export default function Row({ guess }) {
     );
   }
 
-  return ( //else render empty 
+  if (currentGuess) { //live render current guess
+    let letters = currentGuess.split("");
+    
+    return (
+      <div className="row current">
+        {letters.map((letter, i) => (
+          <div key={i} className="filled">
+            {letter}
+          </div>
+        ))}
+        {[...Array(5 - letters.length)].map((v, i) => (
+          <div key={i}></div>
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    //else render empty
     <div className="row">
       <div></div>
       <div></div>
